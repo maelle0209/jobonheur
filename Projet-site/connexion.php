fe411445b946aa6e0724121fb703bd4adf804d7f
Liza-maker <lizasamujlova01@gmail.com>
2024-11-08 12:29:35 +0100

    ajout connexion.php

---
+++ Projet-site/connexion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/nouveau.css" type="text/css" media="screen" />
+    <title>Connexion</title>
+</head>
+<body>
+
+<h1>Connexion</h1>
+<p>Pas encore de compte ? <a href="nouveau.php">Créer un compte</a></p>
+
+<form method="post" action="connecteur.php">
+    <p>
+        Adresse e-mail :
+        <input type="email" name="mail" required>
+    </p>
+    <p>
+        Mot de passe :
+        <input type="password" name="mdp" required>
+    </p>
+    <p>
+        <input type="submit" value="Se connecter">
+    </p>
+</form>
+
+</body>
+</html>
