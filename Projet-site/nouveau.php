bce6ed3e430b86c92f21eea49771947dc4b5e7bb
Liza-maker <lizasamujlova01@gmail.com>
2024-12-01 23:27:43 +0100

    Changement de css de connexion

--- Projet-site/nouveau.php
+++ Projet-site/nouveau.php
@@ -12,32 +12,31 @@ media="screen" />
 
     <h1>Nouveau Client</h1>
     <p>Création de Compte - Nouveau Client</p>
-    <form method="get" action="enregistrement.php" autocomplet="off">
- 
-    <p>
-            Nom :
-            <input type="text" name="n" value="<?php echo isset($_GET['n']) ? htmlspecialchars($_GET['n']) : ''; ?>"/>
-        </p>
-        <p>
-            Prénom :
-            <input type="text" name="p" value="<?php echo isset($_GET['p']) ? htmlspecialchars($_GET['p']) : ''; ?>"/>
-        </p>
-       
-        <p>
-            Adresse e-mail :
-            <input type="email" name="mail" value="<?php echo isset($_GET['mail']) ? htmlspecialchars($_GET['mail']) : ''; ?>"/>
-        </p>
-        <p>
-            Mot de passe :
-            <input type="password" name="mdp1" value=""/>
-        </p>
-        <p>
-            Confirmer votre mot de passe :
-            <input type="password" name="mdp2" value=""/>
-        </p>
-        <p>
-            <input type="submit" value="Envoyer">
-        </p>
-    </form>
+    <form method="get" action="enregistrement.php" autocomplete="off">
+     <p>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="n" value="<?php echo isset($_GET['n']) ? htmlspecialchars($_GET['n']) : ''; ?>">
+     </p>
+     <p>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="p" value="<?php echo isset($_GET['p']) ? htmlspecialchars($_GET['p']) : ''; ?>">
+     </p>
+     <p>
+        <label for="email">Adresse e-mail :</label>
+        <input type="email" id="email" name="mail" value="<?php echo isset($_GET['mail']) ? htmlspecialchars($_GET['mail']) : ''; ?>">
+     </p>
+     <p>
+        <label for="password1">Mot de passe :</label>
+        <input type="password" id="password1" name="mdp1">
+     </p>
+     <p>
+        <label for="password2">Confirmer votre mot de passe :</label>
+        <input type="password" id="password2" name="mdp2">
+     </p>
+     <p>
+        <input type="submit" value="Envoyer">
+     </p>
+</form>
+
 </body>
 </html>
