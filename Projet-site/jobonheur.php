da9706caad0b57c918a7c3116d8a6fdebbb9c696
Liza-maker <lizasamujlova01@gmail.com>
2024-12-03 17:10:08 +0100

    Changement de jobonheur.php

--- Projet-site/jobonheur.php
+++ Projet-site/jobonheur.php
@@ -23,15 +23,17 @@
     <div class="info-section">
         <div class="info-box">
             <h3>Qui sommes nous?</h3>
-            <p>Description de l'entreprise.</p>
+            <p>Nous sommes des étudiantes qui sont pousser dans le monde du travail dans savoir comment ça marche. On alors vu le besoin qu’il y a et nous avons décidé de créer espace très éducatif et avec une plate-forme très éclairé qui va vous permettre de mieux comprendre et avoir accès à tous ce qui est nécessaire. Tout ça au même endroit !
+            </p>
         </div>
         <div class="info-box">
             <h3>Créer son CV :</h3>
-            <p>Outils pour créer un CV personnalisé.</p>
+            <p>Notre plate-forme en plus d’apporter une dynamique nouvelle propose un espace dédié à la création de CV, plus besoin d’autres outils pour le mettre en page.
+            </p>
         </div>
         <div class="info-box">
             <h3>Comparateur</h3>
-            <p>Comparer différentes offres d'emploi.</p>
+            <p>Notre innovation de base sur le fait que vous pouvez trouver un type de job selon voiture âge et des habitudes de vies quotidiennes. Si vous avez un job plus stimulant ou alors plus reposant c’est chez nous que vous pouvez le trouver !</p>
         </div>
     </div>
 
